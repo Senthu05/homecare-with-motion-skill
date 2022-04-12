72c315f911b3ac3b871bd3e2702f1706b0a607a7
Senthuran Puthirarajah <69337831+Senthu05@users.noreply.github.com>
2022-04-12 12:13:32 +0930

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -61,10 +61,10 @@ class HomecareWithMotion(MycroftSkill):
         # current_hour = datetime.now_local().time()
 
         # check both condition 1 hour gap and bedtime
+        global gap_reset
         if gap.total_seconds() > 30 and (wake_timeHour < current_hour < bed_timeHour) and gap_reset:
-            self.speak_dialog("confirm.motion")
-            #global gap_reset
-            #gap_reset = False
+            self.speak_dialog("confirm.motion") 
+            gap_reset = False
 
     @intent_file_handler('motion.with.homecare.intent')
     def handle_motion_with_homecare(self, message):
